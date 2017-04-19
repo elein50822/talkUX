4dcf13ad585f5ee55b3f2dfb0b8a6c99619e59b8
ElaineZeng <elein50822@gmail.com>
2017-04-20 00:26:36 +0800

    1.Sponsorship Pack & email form 放好了 2. 上傳兩個php檔案

---
+++ php/download-form.php
@@ -0,0 +1,17 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['download'])){
+    $file = $_SERVER["DOCUMENT_ROOT"].'/TalkUX2017Sponsorship-Eng-Version.pdf';
+    //echo basename($file);
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/pdf');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    //header("Content-Type: application/download");
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+}
+?>
