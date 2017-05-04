00eb6e0b7a95584ad6a4274e7397dce51b6332fd
ElaineZeng <elein50822@gmail.com>
2017-05-04 23:08:14 +0800

    更改圖片、新增call for spreaker、文字大小、地圖排版、部分ＲＷＤ
    修改文字大小

--- js/init.js
+++ js/init.js
@@ -23,21 +23,21 @@ $(function () {
 	 * WOW
 	 */
 
-	new WOW({
-		 mobile: false,
-	}).init();
+	// new WOW({
+	// 	 mobile: false,
+	// }).init();
 
 
 	/* ---------------------------------------------------------
 	 * Team carousel
 	 */
 
-	$("#teamCarousel").owlCarousel({
-		items: 4,
-		itemsTablet: [768,3],
-		itemsTabletSmall: [690,2],
-		itemsMobile : [480,1]
-	});
+	// $("#teamCarousel").owlCarousel({
+	// 	items: 4,
+	// 	itemsTablet: [768,3],
+	// 	itemsTabletSmall: [690,2],
+	// 	itemsMobile : [480,1]
+	// });
 
 	/* ---------------------------------------------------------
 	 * Scroll arrow
@@ -92,113 +92,113 @@ $(function () {
 	});
 	/* Signup form */
 
-	$('#signupForm').bootstrapValidator({
-		message: 'This value is not valid',
-		feedbackIcons: {
-			valid: 'fa fa-check',
-			invalid: 'fa fa-times',
-			validating: 'fa fa-refresh'
-		},
-		submitHandler: function (validator, form, submitButton) {
-			var l = Ladda.create(submitButton[0]),
-				btnText = submitButton.children(".ladda-label");
+	// $('#signupForm').bootstrapValidator({
+	// 	message: 'This value is not valid',
+	// 	feedbackIcons: {
+	// 		valid: 'fa fa-check',
+	// 		invalid: 'fa fa-times',
+	// 		validating: 'fa fa-refresh'
+	// 	},
+	// 	submitHandler: function (validator, form, submitButton) {
+	// 		var l = Ladda.create(submitButton[0]),
+	// 			btnText = submitButton.children(".ladda-label");
 
-			l.start();
-			btnText.html("Sign up...");
+	// 		l.start();
+	// 		btnText.html("Sign up...");
 
-			$.post(form.attr('action'), form.serialize(), function(result) {
-				btnText.html(result.message);
-			}, 'json')
-			.always(function() {
-				l.stop();
-				//btnText.html("Confirmed");
-				validator.disableSubmitButtons(true);
-			});
-		},
-		fields: {
-			signup_email: {
-				validators: {
-					notEmpty: {
-						message: 'Email cannot be empty'
-					},
-					emailAddress: {
-						message: 'The input is not a valid email address'
-					}
-				}
-			}
-		}
-	});
+	// 		$.post(form.attr('action'), form.serialize(), function(result) {
+	// 			btnText.html(result.message);
+	// 		}, 'json')
+	// 		.always(function() {
+	// 			l.stop();
+	// 			//btnText.html("Confirmed");
+	// 			validator.disableSubmitButtons(true);
+	// 		});
+	// 	},
+	// 	fields: {
+	// 		signup_email: {
+	// 			validators: {
+	// 				notEmpty: {
+	// 					message: 'Email cannot be empty'
+	// 				},
+	// 				emailAddress: {
+	// 					message: 'The input is not a valid email address'
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// });
 
 	/* Contact form */
 
-	$('#contactForm').bootstrapValidator({
-		fields: {
-			name: {
-				validators: {
-					notEmpty: {
-						message: 'Name cannot be empty'
-					},
-					stringLength: {
-						min: 6,
-						max: 30,
-						message: 'Name must be more than 6 and less than 30 characters long'
-					},
-					regexp: {
-						regexp: /^[a-zA-Z\s]+$/,
-						message: 'Name can only consist alphabetical characters'
-					}
-				}
-			},
-			contactEmail: {
-				validators: {
-					notEmpty: {
-						message: 'Email cannot be empty'
-					},
-					emailAddress: {
-						message: 'The input is not a valid email address'
-					}
-				}
-			},
-			message: {
-				validators: {
-					notEmpty: {
-						message: 'Message cannot be empty'
-					}
-				}
-			}
-		},
-		feedbackIcons: {
-			valid: 'fa fa-check',
-			invalid: 'fa fa-times',
-			validating: 'fa fa-refresh'
-		},
-		submitHandler: function (validator, form, submitButton) {
-			var l = Ladda.create(submitButton[0]),
-				btnText = submitButton.children(".ladda-label");
+// 	$('#contactForm').bootstrapValidator({
+// 		fields: {
+// 			name: {
+// 				validators: {
+// 					notEmpty: {
+// 						message: 'Name cannot be empty'
+// 					},
+// 					stringLength: {
+// 						min: 6,
+// 						max: 30,
+// 						message: 'Name must be more than 6 and less than 30 characters long'
+// 					},
+// 					regexp: {
+// 						regexp: /^[a-zA-Z\s]+$/,
+// 						message: 'Name can only consist alphabetical characters'
+// 					}
+// 				}
+// 			},
+// 			contactEmail: {
+// 				validators: {
+// 					notEmpty: {
+// 						message: 'Email cannot be empty'
+// 					},
+// 					emailAddress: {
+// 						message: 'The input is not a valid email address'
+// 					}
+// 				}
+// 			},
+// 			message: {
+// 				validators: {
+// 					notEmpty: {
+// 						message: 'Message cannot be empty'
+// 					}
+// 				}
+// 			}
+// 		},
+// 		feedbackIcons: {
+// 			valid: 'fa fa-check',
+// 			invalid: 'fa fa-times',
+// 			validating: 'fa fa-refresh'
+// 		},
+// 		submitHandler: function (validator, form, submitButton) {
+// 			var l = Ladda.create(submitButton[0]),
+// 				btnText = submitButton.children(".ladda-label");
 
-			l.start();
-			btnText.html("Sending...");
+// 			l.start();
+// 			btnText.html("Sending...");
 
-			$.post(form.attr('action'), form.serialize(), function(result) {
-				if(result.sent){
-					btnText.html("Sent!");
-				}
-				else{
-					btnText.html("Error!");
-				}
+// 			$.post(form.attr('action'), form.serialize(), function(result) {
+// 				if(result.sent){
+// 					btnText.html("Sent!");
+// 				}
+// 				else{
+// 					btnText.html("Error!");
+// 				}
 
-				// Reset form after 5s
-				setTimeout(function() {
-					btnText.html("Submit");
-					$(form[0])[0].reset();
-					validator.resetForm();
-				}, 5000);
+// 				// Reset form after 5s
+// 				setTimeout(function() {
+// 					btnText.html("Submit");
+// 					$(form[0])[0].reset();
+// 					validator.resetForm();
+// 				}, 5000);
 
-			}, 'json')
-			.always(function() {
-				l.stop();
-				validator.disableSubmitButtons(true);
-			});
-		},
-	});
+// 			}, 'json')
+// 			.always(function() {
+// 				l.stop();
+// 				validator.disableSubmitButtons(true);
+// 			});
+// 		},
+// 	});
 });
