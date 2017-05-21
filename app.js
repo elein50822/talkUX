96a6bc53f79b13d3bb70e416efb41463f5a290f9
root <citysite1025@gmail.com>
2017-05-21 15:14:56 +0000

    wqwqUpdate app.js

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ app.use(function(err, req, res, next) {
   res.render('error_500');
 });
 
-app.listen(3000, function(){
-    console.log('Listening on port 3000');
+app.listen(8080, function(){
+    console.log('Listening on port 8080');
 });
 
 module.exports = app;
